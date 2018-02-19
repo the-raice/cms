2c4e7fc68a7d38303ea47e776fe4f529df3ff114
Alexander Isik <isik.alexander@yandex.ru>
2018-02-19 07:49:28 +0300

    Create Home.php

---
+++ protected/Controllers/Home.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+use Raice\Controller as Controller;
+    
+class Home
+    extends Controller
+{
+    
+    public function index ()
+    {
+
+        session_start();
+    
+       if ( !empty( $_SESSION['authorized'] ) ) {
+           
+           header('Location: /dashboard');
+           
+       } else {
+        
+           parent::getView( $this->getName() );
+           
+       }
+        
+    }
+    
+}
