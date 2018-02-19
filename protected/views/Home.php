936bfa26bd6aa22f1d068ae2e69bd05f47a2e024
Alexander Isik <isik.alexander@yandex.ru>
2018-02-19 08:01:22 +0300

    Create Home.php

---
+++ protected/views/Home.php
@@ -0,0 +1,16 @@
+        <div class="content__home">
+            <nav class="home__nav">
+                <ul class="nav__ul">
+                    <a class="nav__link" href="/"><li class="nav__li nav__link___choosen">Главная</li></a>
+                    <a class="nav__link" href="/signup"><li class="nav__li">Регистрация</li></a>
+                    <a class="nav__link" href="/signin"><li class="nav__li">Войти</li></a>
+                </ul>
+            </nav>
+            <div class="home__header">
+                <h1 class="home__title">Зарегистрируйтесь и получите все преимущества The Raice</h1>
+                <a class="home__link" href="/signup"><button class="home__button">Попробовать</button></a>
+            </div>
+        </div>
+        <header class="content__header">
+            <h1 class="content__header___title">Основные возможности</h1>
+        </header>
