4fd95cc06301f4470a3085fb35ab0750ad7f8dc9
Alexander Isik <isik.alexander@yandex.ru>
2018-02-19 07:54:49 +0300

    Create User.php

---
+++ protected/Models/User.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Models;
+
+use Raice\Model as Model;
+        
+class User 
+    extends Model
+{
+
+    const TABLE = 'users';
+    
+    public $id;
+    public $role;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $image;
+    public $username;
+    public $password;
+    
+    public static function validateSignup ( $data )
+    {
+        
+        foreach ( $data as $k => $v ) {
+
+            $$k = strtolower(strip_tags(trim($v)));
+            
+        }
+        
+        if ( $password !== $repassword ) {
+            
+            \Controllers\Signup::$email = $email;
+            \Controllers\Signup::$username = $username;
+            \Controllers\Signup::$error = 'Введённые пароли не совпадают!';
+            
+        } else {
+            
+            $byEmail = self::getOneByField( $email, 'email' );
+            $byUsername = self::getOneByField( $username, 'username' );
+            
+            if ( empty( $byUsername ) && empty( $byEmail ) ) {
+                
+                if ( strlen( $password ) < 6 ) {
+                    
+                    \Controllers\Signup::$email = $email;
+                    \Controllers\Signup::$username = $username;
+                    \Controllers\Signup::$error = 'Длина пароля должная быть не менее 6 символов!';
+                    
+                } else {
+            
+            
+                    if ( !preg_match("/^[\w\d\s.,-]*$/", $username) ) {
+                        
+                        \Controllers\Signup::$email = $email;
+                        \Controllers\Signup::$username = $username;
+                        \Controllers\Signup::$error = 'Имя пользователя должно быть написано латинскими буквами!';
+                        
+                    } else {
+                        
+                        $password = sha1( 'g64%$^*&*_' . $password . 'nHGH6654$%^' );
+                        
+                        $sql = "'', 'user', '', '', '$email', '/assets/images/user.png', '$username', '$password'";
+                        self::insert( $sql );
+                        
+                        $user = self::getOneByField($email, 'email');
+                        
+                        session_start();
+                        $_SESSION['authorized'] = $user[0][id];
+                        
+                        header("Location: /");
+                    
+                    }
+                    
+                }
+            
+            }  elseif ( !empty( $byUsername ) ) {
+                
+                \Controllers\Signup::$email = $email;
+                \Controllers\Signup::$username = $username;
+                \Controllers\Signup::$error = 'Данное имя пользователя уже занято!';
+                
+            } elseif ( !empty( $byEmail ) )  {
+                
+                \Controllers\Signup::$email = $email;
+                \Controllers\Signup::$username = $username;
+                \Controllers\Signup::$error = 'Пользователь с таким адресом электронной почты уже зарегистрирован!';
+                
+            }
+        
+        }
+        
+        
+    }
+    
+    public static function validateSignin ( $data )
+    {
+        
+        foreach ( $data as $k => $v ) {
+
+            $$k = strtolower(strip_tags(trim($v)));
+            
+        }
+        
+        $password = sha1( 'g64%$^*&*_' . $password . 'nHGH6654$%^' );
+
+        $byEmail = self::getOneByTwoFields( $login, 'email', $password, 'password' );
+        $byUsername = self::getOneByTwoFields( $login, 'username', $password, 'password' );
+        
+        if ( empty( $byUsername ) && empty( $byEmail ) ) {
+        
+            \Controllers\Signin::$login = $login;
+            \Controllers\Signin::$error = 'Данные не совпадают!';
+        
+        }  elseif ( !empty( $byUsername ) || !empty( $byEmail ) ) {
+            
+            $user = self::getOneByField($login, 'email') ? self::getOneByField($login, 'email') : self::getOneByField($login, 'username');
+            
+            session_start();
+            $_SESSION['authorized'] = $user[0][id];
+            
+            header("Location: /");   
+            
+        }
+        
+    }
+    
+    public function isEditor( $id, $author_id )
+    {
+        
+        session_start();
+        
+        $user = self::getOneById( $_SESSION['authorized'] );
+        
+        if ( ( $author_id == $_SESSION['authorized'] ) || ( $user['role'] == 'editor' ) ) {
+         
+            return true;
+            
+        } else {
+            
+            return false;
+            
+        }
+        
+    }
+
+}
