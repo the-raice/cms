0da945c8f8dbdd0e86d5d9a0dc47abaeed5cffe0
Alexander Isik <isik.alexander@yandex.ru>
2018-02-19 08:05:43 +0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The Raice Framework.
+ *
+ * @link 
+ * @copyright Copyright (c) 2017 The Raice Framework
+ * @license 
+ */
+ 
+require '../env.php';
+require __DIR__ . '/../autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Raice\Bootstrap();
