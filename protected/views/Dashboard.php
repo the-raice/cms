cd626d71161045eb47da991d8f736d77e32ba546
Alexander Isik <isik.alexander@yandex.ru>
2018-02-24 12:21:02 +0300

    Update Dashboard.php

--- protected/views/Dashboard.php
+++ protected/views/Dashboard.php
@@ -5,7 +5,7 @@
                     <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
                     <a class="nav__link" href="/pages"><li class="nav__li">Страницы</li></a>
                     <a class="nav__link" href="/articles"><li class="nav__li">Статьи</li></a>
-                    <a class="nav__link" href="/profile"><li class="nav__li nav__li___user"><?=\Controllers\Dashboard::$user['username'] ?></li></a>
+                    <a class="nav__link" href="/user/<?=\Controllers\Dashboard::$user['username'] ?>"><li class="nav__li nav__li___user"><?=\Controllers\Dashboard::$user['username'] ?></li></a>
                 </ul>
             </nav>
         </div>
@@ -15,27 +15,45 @@
 <?php  foreach ( \Controllers\Dashboard::$notifications as $notification ): ?>
                 <article class="content__article">
                     <div class="article__column article__column___top">
-<?php if ( !empty( strpos( $notification['action_url'], 'article') ) ): ?>
-<?php $title = mb_strtolower( \Models\Article::getOneByField( str_replace( '/article/', '', $notification['action_url']), 'url' )[0]['title'] ); ?>
-<?php $username = \Models\User::getOneById( \Models\Article::getOneByField( str_replace( '/article/', '', $notification['action_url']), 'url' )[0]['author_id'] )[0]['username']; ?>
+    <?php if ( !empty( strpos( $notification['action_url'], 'article') ) && empty( strpos( $notification['action_url'], 'comment') ) ): ?>
+        <?php $title = mb_strtolower( \Models\Article::getOneByField( str_replace( '/article/', '', $notification['action_url']), 'url' )[0]['title'] ); ?>
+        <?php $username = \Models\User::getOneById( $notification['author_id'] )[0]['username']; ?>
                         <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?> <a class="article__link" href="<?=$notification['action_url'] ?>"><?=$title ?></a>
-<?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
+    <?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
                         <a class="metabar__option___delete" href="#">x</a></h3>
-<?php endif; ?>  
-<?php elseif ( !empty( strpos ( $notification['action_url'], 'user') ) ): ?>
-<?php $username = mb_strtolower( \Models\User::getOneByField( str_replace( '/user/', '', $notification['action_url']), 'username' )[0]['username'] ); ?>
+    <?php endif; ?>  
+    <?php elseif ( !empty( strpos ( $notification['action_url'], 'user') ) ): ?>
+        <?php $username = \Models\User::getOneById( $notification['author_id'] )[0]['username']; ?>
                         <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?></a>
-<?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
+        <?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
                         <a class="metabar__option___delete" href="#">x</a></h3>
-<?php endif; ?>
-<?php else: ?>
-<?php $title = mb_strtolower( \Models\Page::getOneByField( $notification['action_url'], 'url' )[0]['title'] ); ?>
-<?php $username = \Models\User::getOneById( \Models\Page::getOneByField( str_replace( '/article/', '', $notification['action_url']), 'url' )[0]['author_id'] )[0]['username']; ?>
-                        <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?> <a class="article__link" href="<?=$notification['action_url'] ?>"><?=$title ?></a>
-<?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
+        <?php endif; ?>
+    <?php elseif ( !empty( strpos( $notification['action_url'], 'comment') ) ): ?>
+        <?php if ( !empty( strpos( $notification['action_url'], 'article') ) ): ?>
+            <?php $url = str_replace('/comment', '', $notification['action_url']); ?>
+            <?php $title = mb_strtolower( \Models\Article::getOneByField( str_replace( '/comment/article/', '', $notification['action_url']), 'url' )[0]['title'] ); ?>
+            <?php $username = \Models\User::getOneById( $notification['author_id'] )[0]['username']; ?>
+                        <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?> <a class="article__link" href="<?=$url ?>"><?=$title ?></a>
+            <?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
                         <a class="metabar__option___delete" href="#">x</a></h3>
-<?php endif; ?>
-<?php endif;?>
+            <?php endif; ?>
+        <?php else: ?>
+            <?php $title = mb_strtolower( \Models\Page::getOneByField( str_replace( '/comment/', '', $notification['action_url']), 'url' )[0]['title'] ); ?>
+            <?php $url = str_replace('/comment', '', $notification['action_url']); ?>
+            <?php $username = \Models\User::getOneById( $notification['author_id'] )[0]['username']; ?>
+                        <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?> <a class="article__link" href="<?=$url ?>"><?=$title ?></a>
+            <?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
+                        <a class="metabar__option___delete" href="#">x</a></h3>
+            <?php endif; ?>
+        <?php endif; ?>
+    <?php else: ?>
+        <?php $title = mb_strtolower( \Models\Page::getOneByField( $notification['action_url'], 'url' )[0]['title'] ); ?>
+        <?php $username = \Models\User::getOneById( $notification['author_id'] )[0]['username']; ?>
+                        <h3 class="article__title"><a href="/user/<?=$username ?>">@<?=$username ?></a> <?=$notification['content'] ?> <a class="article__link" href="/<?=$notification['action_url'] ?>"><?=$title ?></a>
+        <?php if ( Models\User::isEditor( $_SESSION['authorized'], $notification['author_id'] ) ): ?>
+                        <a class="metabar__option___delete" href="#">x</a></h3>
+        <?php endif; ?>
+    <?php endif;?>
 
                 </div>
                 <div class="article__column">
