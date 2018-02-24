03ae0f09ece6d72ee7f1df6e874a58bf87065303
Alexander Isik <isik.alexander@yandex.ru>
2018-02-24 12:28:34 +0300

    Update Page.php

--- protected/views/Page.php
+++ protected/views/Page.php
@@ -7,7 +7,7 @@
                     <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
                     <a class="nav__link" href="/pages"><li class="nav__li nav__link___choosen">Страницы</li></a>
                     <a class="nav__link" href="/articles"><li class="nav__li">Статьи</li></a>
-                    <a class="nav__link" href="/profile"><li class="nav__li nav__li___user"><?=$class::$user['username'] ?></li></a>
+                    <a class="nav__link" href="/user/<?=$class::$user['username'] ?>"><li class="nav__li nav__li___user"><?=$class::$user['username'] ?></li></a>
 <?php else: ?>
                     <a class="nav__link" href="/signin"><li class="nav__li">Войти</li></a>
                     <a class="nav__link" href="/signup"><li class="nav__li">Регистрация</li></a>
