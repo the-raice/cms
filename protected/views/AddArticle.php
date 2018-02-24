d1691054f31be3d6a834ca15ca13f8f43c035000
Alexander Isik <isik.alexander@yandex.ru>
2018-02-24 12:21:58 +0300

    Update AddArticle.php

--- protected/views/AddArticle.php
+++ protected/views/AddArticle.php
@@ -2,10 +2,10 @@
             <nav class="addarticle__nav">
                 <ul class="nav__ul">
                     <a class="nav__link" href="/dashboard"><li class="nav__li">Панель управления</li></a>
-                    <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
+                    <a class="nav__link" href="/add"><li class="nav__li nav__link___choosen">Добавить</li></a>
                     <a class="nav__link" href="/pages"><li class="nav__li">Страницы</li></a>
-                    <a class="nav__link" href="/articles"><li class="nav__li nav__link___choosen">Статьи</li></a>
-                    <a class="nav__link" href="/profile"><li class="nav__li nav__li___user"><?=Models\User::getOneById( \Controllers\Add::$user )[0]['username'] ?></li></a>
+                    <a class="nav__link" href="/articles"><li class="nav__li">Статьи</li></a>
+                    <a class="nav__link" href="/user/<?=Models\User::getOneById( \Controllers\Add::$user )[0]['username'] ?>"><li class="nav__li nav__li___user"><?=Models\User::getOneById( \Controllers\Add::$user )[0]['username'] ?></li></a>
                 </ul>
             </nav>
         </div>
