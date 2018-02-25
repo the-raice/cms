2285a632b4fbc9d37c50ec5dbaae8e55e6b04711
Alexander Isik <isik.alexander@yandex.ru>
2018-02-25 07:38:37 +0300

    Update Articles.php

--- protected/Controllers/Articles.php
+++ protected/Controllers/Articles.php
@@ -3,34 +3,75 @@
 namespace Controllers;
 
 use Raice\Controller as Controller;
-use Models\Article as Article;
 use Models\User as User;
     
-class Articles
+class Comment
     extends Controller
 {
     
     
-    public static $articles;
+    public static $page;
     public static $user;
+    public static $comment;
+    public static $id;
     
     public function index ()
     {
         
         session_start();
         
-        self::$articles = Article::getAll();
-    
-        if ( !empty( $_SESSION['authorized'] ) ) {
-               
-            self::$user = \Models\User::getOneById( $_SESSION['authorized'] )[0];
-               
-            parent::getView( $this->getName(), 'Статьи' );
-               
+        $action = explode('/', $_SERVER['REQUEST_URI'])[3];
+        self::$id = explode('/', $_SERVER['REQUEST_URI'])[2];
+        self::$comment = \Models\Comments::getOneById( self::$id )[0];
+      
+        self::$user = \Models\User::getOneById( $_SESSION['authorized'] )[0];
+        
+        if ( !empty( self::$user ) && !empty( self::$comment ) ) {
+        
+            if ( $action == 'edit' || $action == 'delete' ) {
+                
+                $this->$action();
+                
+            }
+        
         } else {
-               
-            header('Location: /');
-               
+            
+            header("Location: /");
+            
+        }
+        
+    }
+    
+    public function delete()
+    {
+        
+        if ( !empty( $_POST ) ) {
+
+                \Models\Comments::delete( self::$id, 'id');
+
+                
+        } else {
+            
+            parent::getView('CommentDelete', 'Удаление комментария');
+            
+        }
+        
+    }
+    
+    public function edit()
+    {
+
+        if ( !empty( $_POST ) ) {
+
+            $comment = $_POST['comment'];
+            $date = date("Y-m-d");
+            var_dump(\Models\Comments::update("content='" . $comment . "',date='" . $date . "'", self::$id, 'id'));
+
+                
+        } else {
+            
+            parent::getView('CommentEdit', 'Редактирование комментария');
+            
         }
         
     }
