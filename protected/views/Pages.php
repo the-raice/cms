af58d23f138f0fb811b2c4c4a446a4a3e82eae9d
Alexander Isik <isik.alexander@yandex.ru>
2018-02-26 11:56:01 +0300

    Update Pages.php

--- protected/views/Pages.php
+++ protected/views/Pages.php
@@ -1,10 +1,3 @@
-        <div class="content__pages">
-            <nav class="pages__nav">
-                <ul class="nav__ul">
-                    <a class="nav__link" href="/dashboard"><li class="nav__li">Панель управления</li></a>
-                    <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
-                    <a class="nav__link" href="/pages"><li class="nav__li nav__link___choosen">Страницы</li></a>
-                    <a class="nav__link" href="/articles"><li class="nav__li">Статьи</li></a>
                     <a class="nav__link" href="/user/<?=\Controllers\Pages::$user['username'] ?>"><li class="nav__li nav__li___user"><?=\Controllers\Pages::$user['username'] ?></li></a>
                 </ul>
             </nav>
