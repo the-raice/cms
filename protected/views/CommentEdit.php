2f8003288337938437eb7c00690c816d145cd332
Alexander Isik <isik.alexander@yandex.ru>
2018-02-26 11:52:09 +0300

    Update CommentEdit.php

--- protected/views/CommentEdit.php
+++ protected/views/CommentEdit.php
@@ -1,10 +1,3 @@
-        <div class="content__commentedit">
-            <nav class="commentedit__nav">
-                <ul class="nav__ul">
-                    <a class="nav__link" href="/dashboard"><li class="nav__li nav__link___choosen">Панель управления</li></a>
-                    <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
-                    <a class="nav__link" href="/pages"><li class="nav__li">Страницы</li></a>
-                    <a class="nav__link" href="/articles"><li class="nav__li">Статьи</li></a>
                     <a class="nav__link" href="/user/<?=\Controllers\Comment::$user['username'] ?>"><li class="nav__li nav__li___user"><?=\Controllers\Comment::$user['username'] ?></li></a>
                 </ul>
             </nav>
