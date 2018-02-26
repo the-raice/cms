62b0907632ed06163eae087c97769660594b0b40
Alexander Isik <isik.alexander@yandex.ru>
2018-02-26 11:55:36 +0300

    Update PageEdit.php

--- protected/views/PageEdit.php
+++ protected/views/PageEdit.php
@@ -1,10 +1,3 @@
-        <div class="content__pageedit">
-            <nav class="pageedit__nav">
-                <ul class="nav__ul">
-                    <a class="nav__link" href="/dashboard"><li class="nav__li">Панель управления</li></a>
-                    <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
-                    <a class="nav__link" href="/pages"><li class="nav__li">Страницы</li></a>
-                    <a class="nav__link" href="/articles"><li class="nav__li nav__link___choosen">Статьи</li></a>
 <?php $class = '\Controllers\\' . explode('/', str_replace( '-', '', $_SERVER['REQUEST_URI']) )[1]; ?>
                     <a class="nav__link" href="/user/<?=$class::$user['username'] ?>"><li class="nav__li nav__li___user"><?=$class::$user['username'] ?></li></a>
                 </ul>
