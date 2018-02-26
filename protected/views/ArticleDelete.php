3525a19727a05bb6fb9d1fd487d026a84c48d85d
Alexander Isik <isik.alexander@yandex.ru>
2018-02-26 11:49:20 +0300

    Update ArticleDelete.php

--- protected/views/ArticleDelete.php
+++ protected/views/ArticleDelete.php
@@ -1,10 +1,3 @@
-        <div class="content__articledelete">
-            <nav class="articledelete__nav">
-                <ul class="nav__ul">
-                    <a class="nav__link" href="/dashboard"><li class="nav__li">Панель управления</li></a>
-                    <a class="nav__link" href="/add"><li class="nav__li">Добавить</li></a>
-                    <a class="nav__link" href="/pages"><li class="nav__li">Страницы</li></a>
-                    <a class="nav__link" href="/articles"><li class="nav__li nav__link___choosen">Статьи</li></a>
                     <a class="nav__link" href="/user/<?=\Controllers\Article::$user['username'] ?>"><li class="nav__li nav__li___user"><?=\Controllers\Article::$user['username'] ?></li></a>
                 </ul>
             </nav>
