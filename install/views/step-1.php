c4ba87f50fe7821bc4638059031c778d2eca778f
Alexander Isik <isik.alexander@yandex.ru>
2018-03-29 02:36:12 +0300

    Create step-1.php

---
+++ install/views/step-1.php
@@ -0,0 +1,32 @@
+                <form method="post" action="">
+                    <h1 class="content__header___title">Настройка базы данных</h1>
+                    <?php if ( !empty( $error ) ): ?>
+                    <div class="error"><?=$error ?></div>
+                    <?php endif; ?>
+                    <label class="form__label" for="server">Сервер базы данных:</label>
+                    <br>
+                    <input value="<?=$_POST['host'] ?>" required="" class="form__input" type="text" name="host">
+                    <br>
+                    <label class="form__label" for="server">Имя базы данных:</label>
+                    <br>
+                    <input value="<?=$_POST['name'] ?>" required="" class="form__input" type="text" name="name">
+                    <br>
+                    <label class="form__label" for="server">Имя пользователя базы данных:</label>
+                    <br>
+                    <input value="<?=$_POST['user'] ?>" required="" class="form__input" type="text" name="user">
+                    <br>
+                    <label class="form__label" for="server">Пароль сервера базы данных:</label>
+                    <br>
+                    <input class="form__input" type="password" name="password">
+                    <br>
+                    <br>
+                    <input class="form__submit" value="Перейти к третьему шагу&nbsp;&nbsp;&nbsp;>" type="submit" id="submit">
+                </form>
+            </header>
+        </div>
+    </div>
+    <script src="/public/assets/js/pagePreloadEffectClassie.js"></script>
+	<script src="/public/assets/js/pagePreloadEffectPathLoader.js"></script>
+	<script src="/public/assets/js/pagePreloadEffectMain.js"></script>
+</body>
+</html>
