e4bdd31c6814423863a837d17601628ded72da67
Alexander Isik <isik.alexander@yandex.ru>
2018-03-29 02:42:21 +0300

    Update step-0.php

--- install/views/step-0.php
+++ install/views/step-0.php
@@ -2,11 +2,3 @@
                 <h3>Чтобы установить The Raice CMS, вам понадобиться всего несколько минут. Для этого вы должны знать адрес сервера, где располагается база данных, в которой будут хранится все данные, имя и пароль пользователя, а также произвести регистрацию первичного пользователя.</h3>
                 <br>
                 <a href="/install/install?step=1"><button class="form__submit" type="submit" id="submit">Перейти к первому шагу</button></a>
-            </header>
-        </div>
-    </div>
-    <script src="/public/assets/js/pagePreloadEffectClassie.js"></script>
-	<script src="/public/assets/js/pagePreloadEffectPathLoader.js"></script>
-	<script src="/public/assets/js/pagePreloadEffectMain.js"></script>
-</body>
-</html>
