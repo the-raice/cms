96dca5bafbdb506ea6131dc236caea6f93aeff9d
Alexander Isik <isik.alexander@yandex.ru>
2018-03-29 02:42:41 +0300

    Update step-3.php

--- install/views/step-3.php
+++ install/views/step-3.php
@@ -2,11 +2,3 @@
                 <h3>Установка завершена и теперь вы можете начать использование The Raice CMS.</h3>
                 <br>
                 <a href="/install/install?step=4"><button class="form__submit" type="submit" id="submit">Завершить установку</button></a>
-            </header>
-        </div>
-    </div>
-    <script src="/public/assets/js/pagePreloadEffectClassie.js"></script>
-	<script src="/public/assets/js/pagePreloadEffectPathLoader.js"></script>
-	<script src="/public/assets/js/pagePreloadEffectMain.js"></script>
-</body>
-</html>
