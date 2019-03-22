04c70b1303d9ede75b111363314b1ee42d0eb56e
Alexander Isik <isik.alexander@yandex.ru>
2019-03-22 16:47:49 +1000

    Update Settings.php

--- protected/Controllers/Settings.php
+++ protected/Controllers/Settings.php
@@ -21,9 +21,13 @@ class Settings
            self::$user = \Models\User::getOneById( $_SESSION['authorized'] )[0];
            self::$notifications = \Models\Notifications::getAll();
            
-		   if ( !empty( explode( '/', $_SERVER['REQUEST_URI'] ) ) ) 
+		   if ( !empty( explode( '/', $_SERVER['REQUEST_URI'] )[2] ) ) {
+			   
+			   $this->{explode( '/', $_SERVER['REQUEST_URI'] )[2]}();
+			   
+		   }
 		   
-           parent::getView( $this->getName(), 'Панель управления' );
+           parent::getView( $this->getName(), 'Панель управления: настройки' );
            
        } else {
            
@@ -33,4 +37,26 @@ class Settings
         
     }
     
+	public function main () 
+	{
+		
+		parent::getView( 'SettingsMain', 'Панель управления: настройки' );
+		
+	}
+    
+	public function users () 
+	{
+		
+		parent::getView( 'SettingsUsers', 'Панель управления: настройки' );
+		
+	}
+	
+    
+	public function design () 
+	{
+		
+		parent::getView( 'SettingsDesign', 'Панель управления: настройки' );
+		
+	}
+	
 }
