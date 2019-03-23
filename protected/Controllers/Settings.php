8eadd066304bee05e82fcffd0a347ea4cd203b81
Alexander Isik <isik.alexander@yandex.ru>
2019-03-23 11:15:25 +1000

    Update Settings.php

--- protected/Controllers/Settings.php
+++ protected/Controllers/Settings.php
@@ -9,38 +9,42 @@ class Settings
 {
     
     public static $user;
-    public static $notifications;
+	public static $settings;
+	
+	public function __construct ()
+	{
+		
+		session_start();
+    
+		if ( !empty( $_SESSION['authorized'] ) ) {
+           
+		    self::$user = \Models\User::getOneById( $_SESSION['authorized'] )[0];
+		   
+		} else {
+			
+			header("Location: /");
+			
+		}
+		
+	}
     
     public function index ()
     {
 
-       session_start();
-    
-       if ( !empty( $_SESSION['authorized'] ) ) {
-           
-           self::$user = \Models\User::getOneById( $_SESSION['authorized'] )[0];
-           self::$notifications = \Models\Notifications::getAll();
-           
-		   if ( !empty( explode( '/', $_SERVER['REQUEST_URI'] )[2] ) ) {
-			   
-			   $this->{explode( '/', $_SERVER['REQUEST_URI'] )[2]}();
-			   
-		   }
+	   if ( !empty( explode( '/', $_SERVER['REQUEST_URI'] )[2] ) ) {
 		   
-           parent::getView( $this->getName(), 'Панель управления: настройки' );
+		   $this->{explode( '/', $_SERVER['REQUEST_URI'] )[2]}();
+		   
+	   }
+	   
+	   parent::getView( $this->getName(), 'Панель управления: настройки' );
            
-       } else {
-           
-           header('Location: /');
-           
-       }
-        
     }
     
 	public function main () 
 	{
 		
-		parent::getView( 'SettingsMain', 'Панель управления: настройки' );
+		parent::getView( 'SettingsMain', 'Панель управления: настройки' );var_dump(self::$user);
 		
 	}
     
